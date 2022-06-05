a37a54bc2d1af194313081f38fd5e8bca2bab643
berkanturkali <brktrkali@outlook.com>
2022-06-05 18:32:47 +0300

    add ComingSoonDestination

---
+++ feature-upcomings/src/main/java/com/example/movies_compose/feature/upcomings/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.movies_compose.feature.upcomings.util
+
+object Constants {
+
+    const val COMING_SOON_ROUTE = "coming_soon_route"
+
+    const val COMING_SOON_DESTINATION = "coming_soon_destination"
+}
