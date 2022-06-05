45290097ca3056cee6d5c16d0388c0779ddcd138
berkanturkali <brktrkali@outlook.com>
2022-06-05 18:44:21 +0300

    add DownloadsDestination

---
+++ feature-downloads/src/main/java/com/example/movies_compose/feature/downloads/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.movies_compose.feature.downloads.util
+
+object Constants {
+
+    const val DOWNLOADS_ROUTE = "downloads_route"
+
+    const val DOWNLOADS_DESTINATION = "downloads_destination"
+}
