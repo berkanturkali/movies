3157f942115744bed28f8a9810330128ff28d4eb
berkanturkali <brktrkali@outlook.com>
2022-06-05 18:24:51 +0300

    add SearchDestination

---
+++ feature-search/src/main/java/com/example/movies_compose/feature/search/navigation/SearchDestination.kt
@@ -0,0 +1,23 @@
+package com.example.movies_compose.feature.search.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.movies_compose.core.navigation.MoviesNavigationDestination
+import com.example.movies_compose.feature.search.util.Constants.SEARCH_DESTINATION
+import com.example.movies_compose.feature.search.util.Constants.SEARCH_ROUTE
+
+object SearchDestination : MoviesNavigationDestination {
+    override val route: String
+        get() = SEARCH_ROUTE
+    override val destination: String
+        get() = SEARCH_DESTINATION
+}
+
+fun NavGraphBuilder.addSearchGraph() {
+
+    composable(
+        route = SearchDestination.route
+    ) {
+        //search screen
+    }
+}
---
+++ feature-search/src/main/java/com/example/movies_compose/feature/search/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.movies_compose.feature.search.util
+
+object Constants {
+
+    const val SEARCH_ROUTE = "search_route"
+
+    const val SEARCH_DESTINATION = "search_destination"
+}
