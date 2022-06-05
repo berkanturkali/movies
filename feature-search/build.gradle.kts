ec158cfe632c5fc8f22c4c0c331eed0fd1787e61
berkanturkali <brktrkali@outlook.com>
2022-06-05 17:35:46 +0300

    create feature-search module

---
+++ feature-search/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("movies_compose.android.library")
+    id("movies_compose.android.feature")
+    id("movies_compose.android.library.compose")
+    id("movies_compose.android.library.jacoco")
+    id("movies_compose.spotless")
+}
+
+
+dependencies {
+}
