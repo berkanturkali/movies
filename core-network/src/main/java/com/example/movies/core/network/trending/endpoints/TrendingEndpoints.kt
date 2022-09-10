af0b66bfa30f864279d12b129d05a9130c5ae932
berkanturkali <brktrkali@outlook.com>
2022-09-10 19:41:20 +0300

    add TrendinEndpoints

---
+++ core-network/src/main/java/com/example/movies/core/network/trending/endpoints/TrendingEndpoints.kt
@@ -0,0 +1,12 @@
+package com.example.movies.core.network.trending.endpoints
+
+/**
+ * Trending-related endpoints that The Movie Database API provides.
+ */
+object TrendingEndpoints {
+
+    private const val TRENDING_PATH = "trending"
+
+
+    const val TRENDING_ENDPOINT = TRENDING_PATH
+}
