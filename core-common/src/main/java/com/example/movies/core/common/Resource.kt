d9256e4b960b252caccc44cbf4b69835e45934ed
berkanturkali <brktrkali@outlook.com>
2022-09-24 23:55:19 +0300

    modify Resource & UiText classes

--- core-common/src/main/java/com/example/movies/core/common/Resource.kt
+++ core-common/src/main/java/com/example/movies/core/common/Resource.kt
@@ -1,7 +1,14 @@
 package com.example.movies.core.common
 
 sealed class Resource<T>(
-    val data:T? = null,
-
+    val data: T? = null,
+    val error: UiText? = null,
+    val errorCode: Int? = null,
 ) {
+    class Success<T>(data: T? = null) : Resource<T>(data)
+
+    class Error<T>(code: Int? = null, text: UiText, data: T? = null) :
+        Resource<T>(data = data, errorCode = code, error = text)
+
+    class Loading<T> : Resource<T>(data = null)
 }
--- core-common/src/main/java/com/example/movies/core/common/UiText.kt
+++ core-common/src/main/java/com/example/movies/core/common/UiText.kt
@@ -1,7 +1,20 @@
 package com.example.movies.core.common
 
+import android.content.Context
+import androidx.annotation.StringRes
+
 sealed class UiText {
-    data class DynamicString(val value:String?) : UiText()
+    data class DynamicString(val value: String?) : UiText()
 
+    class StringResource(
+        @StringRes val resId: Int,
+        vararg val args: Any
+    ) : UiText()
 
+    fun asString(context: Context): String? {
+        return when (this) {
+            is DynamicString -> value
+            is StringResource -> context.getString(resId, *args)
+        }
+    }
 }
