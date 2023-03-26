bf51a5ac08c7aae6a04bc985a3bd4e557252feb4
bturkali <berkan.turkali@path.com.tr>
2023-03-26 15:34:34 +0300

    add Name composable

---
+++ feature-search/src/main/java/com/example/movies/feature/search/components/Name.kt
@@ -0,0 +1,39 @@
+package com.example.movies.feature.search.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.examle.movies.core.ui.providers.LocalWindowWidthSizeClass
+import com.examle.movies.core.ui.theme.MoviesTheme
+
+@Composable
+fun Name(
+    modifier: Modifier = Modifier,
+    childModifier: Modifier = Modifier,
+    name: String
+) {
+
+    Text(
+        text = name, modifier = modifier.then(childModifier),
+        color = MaterialTheme.colorScheme.onPrimary,
+        style = MaterialTheme.typography.bodySmall,
+        maxLines = 3,
+        overflow = TextOverflow.Ellipsis
+    )
+
+}
+
+@Preview
+@Composable
+fun NamePrev() {
+    CompositionLocalProvider(LocalWindowWidthSizeClass provides WindowWidthSizeClass.Compact) {
+        MoviesTheme {
+            Name(name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco")
+        }
+    }
+}
