265774ef09684d2afc1e900c948f0f9a878c70f6
bturkali <berkan.turkali@path.com.tr>
2023-06-15 22:10:07 +0300

    add ReviewItemUserName composable

---
+++ feature-details/src/main/java/com/example/movies/feature/details/movie/components/ReviewItemUserName.kt
@@ -0,0 +1,32 @@
+package com.example.movies.feature.details.movie.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.examle.movies.core.ui.theme.MoviesTheme
+
+@Composable
+fun ReviewItemUserName(
+    name: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        modifier = modifier,
+        text = name,
+        style = MaterialTheme.typography.titleLarge,
+        color = MaterialTheme.colorScheme.onSurface,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis,
+    )
+}
+
+@Preview
+@Composable
+fun ReviewItemUserNamePrev() {
+    MoviesTheme {
+        ReviewItemUserName(name = "MovieGuys")
+    }
+}
